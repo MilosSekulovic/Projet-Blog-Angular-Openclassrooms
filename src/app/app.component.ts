61398c424e975adb4c9fa04768005712360678ae
Sekulovic <milos.s@hotmail.fr>
2019-04-17 14:18:38 +0200

    Création de la BDD avec firbase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,23 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app'; // a verifier sans le /app
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
 
-  posts = [
-    {
-      title: "Mon Premier Post1",
-      content: "blablablablab",
-      loveIts: 1,
-      created_at: new Date()
-    },
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyDHvSvjd5U5x4_zFTkqIAyffEnNisaRbF0",
+      authDomain: "http-blog-angular.firebaseapp.com",
+      databaseURL: "https://http-blog-angular.firebaseio.com",
+      projectId: "http-blog-angular",
+      storageBucket: "http-blog-angular.appspot.com",
+      messagingSenderId: "738984992100"
+    };
+    firebase.initializeApp(config);
+  }
 
-    {
-      title: "Mon Deuxième Post",
-      content: "blablablablab2",
-      loveIts: -2,
-      created_at: new Date()
-    },
-
-    {
-      title: "Mon Deuxième Post",
-      content: "blablablablab2",
-      loveIts: 1,
-      created_at: new Date()
-    }
-
-    ];
 }
--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Post } from '../model/post';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../model/post.model';
+import { PostsService } from '../services/post.service';
 
 @Component({
   selector: 'app-post-list-item',
   templateUrl: './post-list-item.component.html',
   styleUrls: ['./post-list-item.component.scss']
 })
+
 export class PostListItemComponent implements OnInit {
 
   @Input() postTitle: string;
@@ -14,40 +16,26 @@ export class PostListItemComponent implements OnInit {
   @Input() postLoveIts: number;
   @Input() post: Post;
 
-  constructor() { }
+  constructor(private postService: PostsService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  getContent() {
-    return this.postContent;
-  }
-
-  getColor() {
-    if(this.postLoveIts > 0) {
-      return 'green';
-    } else if (this.postLoveIts < 0) {
-      return 'red';
-    } 
-  }
-
-  // onLoveIt() {
-  //     console.log("Love it !");
-  // }
-
-  // onDontLoveIt() {
-  //     console.log("Don't love it !");
-  // }
-  
   onLoveIt() {
     this.postLoveIts++;
     this.post.loveIts = this.postLoveIts;
-    // this.postService.savePosts();
+    this.postService.savePosts();
   }
 
   onDontLoveIt() {
     this.postLoveIts--;
     this.post.loveIts = this.postLoveIts;
-    // this.postService.savePosts();
+    this.postService.savePosts();
+  }
+
+  onDeletePost(post: Post) {
+    const confirmation = confirm('Voulez-vous supprimer ce post?');
+    if (confirmation) {
+      this.postService.removePost(post);
+    }
   }
 }
